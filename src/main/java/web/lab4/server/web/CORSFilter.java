ca3a4898fc40ac31344483839aa7a68092b8ad15
sultanowskii <dsr431@yandex.ru>
2023-12-06 11:37:52 +0300

    CORS filter

---
+++ src/main/java/web/lab4/server/web/CORSFilter.java
@@ -0,0 +1,18 @@
+package web.lab4.server.web;
+
+import jakarta.ws.rs.container.ContainerRequestContext;
+import jakarta.ws.rs.container.ContainerResponseContext;
+import jakarta.ws.rs.container.ContainerResponseFilter;
+import jakarta.ws.rs.ext.Provider;
+
+@Provider
+public class CORSFilter implements ContainerResponseFilter {
+
+    @Override
+    public void filter(ContainerRequestContext requestContext, ContainerResponseContext responseContext) {
+        var headers = responseContext.getHeaders();
+        headers.add("Access-Control-Allow-Origin", "*");
+        headers.add("Access-Control-Allow-Methods", "GET, POST, DELETE, PUT");
+        headers.add("Access-Control-Allow-Headers", "X-Requested-With, Content-Type");
+    }
+}
